22cd56324b46c706b99395536e6b0e66d64fdb4d
XboxYan <yanwenbin1991@live.com>
2016-09-25 23:30:29 +0800

    vscode

--- .vscode/launchReactNative.js
+++ .vscode/launchReactNative.js
@@ -2,7 +2,7 @@
 // Please do not modify it manually. All changes will be lost.
 try {
     var path = require("path");
-    var Launcher = require("C:\\Users\\文彬\\.vscode\\extensions\\vsmobile.vscode-react-native-0.2.0\\out\\debugger\\launcher.js").Launcher;
+    var Launcher = require("C:\\Users\\yanwe\\.vscode\\extensions\\vsmobile.vscode-react-native-0.2.0\\out\\debugger\\launcher.js").Launcher;
     new Launcher(path.resolve(__dirname, "..")).launch();
 } catch (e) {
     throw new Error("Unable to launch application. Try deleting .vscode/launchReactNative.js and restarting vscode.");
